c4cbf984a85fcab57f376cf6ff9663b6fea58052
John Umeasiegbu <johnbendie@gmail.com>
2024-08-17 05:20:33 +0100

    Fix misssing type infromation

--- lib/cjs/Client/ClientFactory.d.ts
+++ lib/cjs/Client/ClientFactory.d.ts
@@ -4,6 +4,9 @@ import { IncomingMessage, ServerResponse } from "http";
 import { EMR } from "../Launcher/SmartLaunchHandler";
 import { FhirClientConfig } from "../types";
 import BaseClient from "./BaseClient";
+/**
+ * Enum representing the different types of launch configurations.
+ */
 export declare enum LAUNCH {
     EMR = 0,
     STANDALONE = 1,
@@ -13,50 +16,56 @@ export declare enum LAUNCH {
 /**
  * The type represents a JSON Web Token (JWT) with properties for client_id and an optional epic.eci property.
  * @property {string} client_id - A string representing the client ID.
- * @property {string}  - - `client_id`: A string representing the client ID associated with the JWT.
+ * @property {string} [epic.eci] - An optional string representing the Epic ECI (Epic Community Identifier).
  */
 export type JWT = {
     client_id: string;
     "epic.eci"?: string;
 };
 /**
- * The function checks if an object is an instance of the JWT class by verifying if it has a client_id property.
+ * The function checks if an object is an instance of the JWT type by verifying if it has a client_id property.
  * @param {unknown} object - The `object` parameter is of type `unknown`, which means it can be any type.
- * @returns a boolean value.
+ * @returns {boolean} - A boolean value indicating whether the object is an instance of JWT.
  */
 export declare function instanceOfJWT(object: unknown): object is JWT;
 /**
-Represents the ClientFactory class for creating EMR clients.
-*/
+ * Represents the ClientFactory class for creating EMR clients.
+ */
 export default class ClientFactory {
     /**
      * The function `createEMRClient` creates an EMR client based on the specified launch type.
-     * @param {LAUNCH} launchType - The `launchType` parameter is an optional parameter of type `LAUNCH` that specifies the type of EMR launch. It has a default value
-     * of `LAUNCH.EMR`.
-     * @returns a Promise that resolves to an instance of the `BaseClient` class.
+     * @param {LAUNCH} launchType - The `launchType` parameter is an enum of type `LAUNCH` that specifies the type of EMR launch.
+     * @param {EMR} [emrType] - The type of EMR (e.g., EPIC, CERNER, etc.).
+     * @param {string | fhirclient.ClientState} [state] - The state or client state object for the FHIR client. This is used for STATEFUL launches.
+     * @returns {Promise<BaseClient>} - A Promise that resolves to an instance of the `BaseClient` class.
      */
-    createEMRClient(launchType: LAUNCH.EMR | LAUNCH.STANDALONE, emrType?: EMR, state?: string | fhirclient.ClientState): Promise<BaseClient>;
+    createEMRClient(launchType: LAUNCH, emrType?: EMR, state?: string | fhirclient.ClientState): Promise<BaseClient>;
     /**
-     * The function `createEMRClientBackend` creates an EMR client based on the specified launch type.
+     * The function `createEMRClientBackend` creates an EMR client for backend operations.
      * @param {IncomingMessage} req - The `req` parameter is an incoming message object that represents the request made by the client.
      * @param {ServerResponse} res - The `res` parameter is a server response object that represents the response sent by the server.
      * @param {FhirClientConfig} serverConfig - The `serverConfig` parameter is an object that contains the configuration for the FHIR client. It includes the server URL, token response, client ID, and token URI.
-     * @returns a Promise that resolves to an instance of the `BaseClient` class.
+     * @returns {Promise<BaseClient>} - A Promise that resolves to an instance of the `BaseClient` class.
      */
     createEMRClientBackend(req: IncomingMessage, res: ServerResponse, serverConfig: FhirClientConfig): Promise<BaseClient>;
+    /**
+     * The function creates a smarter FHIR client based on the given FHIR client and EMR type.
+     * @param {SubClient} fhirClient - The FHIR client to base the smarter FHIR client on.
+     * @param {EMR} [emrType] - The type of EMR (e.g., EPIC, CERNER, etc.).
+     * @returns {Promise<BaseClient>} - A Promise that resolves to an instance of the `BaseClient` class.
+     */
     private createSmarterFhirClient;
     /**
      * The function creates a default FHIR client based on the launch type.
-     * @param {LAUNCH} launchType - The `launchType` parameter is an enum type called `LAUNCH`. It represents the type of launch for the FHIR client. There are two
-     * possible values for `LAUNCH`:
-     * @returns a Promise that resolves to a SubClient object.
+     * @param {LAUNCH} launchType - The `launchType` parameter is an enum type that represents the type of launch for the FHIR client.
+     * @param {string | fhirclient.ClientState} [state] - The state or client state object for the FHIR client. This is used for STATEFUL launches.
+     * @returns {Promise<SubClient>} - A Promise that resolves to a SubClient object.
      */
     private createDefaultFhirClient;
     /**
-     * The function `getEmrEndpoints` returns the endpoints based on the EMR type obtained from the JWT.
-     * @param {JWT} jwt - The "jwt" parameter is a JSON Web Token (JWT) that is used for authentication and authorization purposes. It contains information about the
-     * user and their permissions.
-     * @returns an object of type EMR_ENDPOINTS.
+     * The function `getEmrEndpoints` returns the endpoints based on the EMR type obtained from the JWT or EMR.
+     * @param {EMR | JWT} object - The object representing either an EMR type or a JWT.
+     * @returns {EMR_ENDPOINTS} - An object containing the EMR endpoints.
      */
     private getEmrEndpoints;
 }
--- src/Client/ClientFactory.ts
+++ src/Client/ClientFactory.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import { fhirclient } from "fhirclient/lib/types";
 import smart, * as FHIR from "fhirclient";
 import { IncomingMessage, ServerResponse } from "http";
@@ -13,6 +14,9 @@ import SmartHealthClient from "./SmartHealthClient";
 import MedplumClient from "./MedplumClient";
 import InfernoClient from "./InfernoClient";
 
+/**
+ * Enum representing the different types of launch configurations.
+ */
 export enum LAUNCH {
   EMR,
   STANDALONE,
@@ -23,7 +27,7 @@ export enum LAUNCH {
 /**
  * The type represents a JSON Web Token (JWT) with properties for client_id and an optional epic.eci property.
  * @property {string} client_id - A string representing the client ID.
- * @property {string}  - - `client_id`: A string representing the client ID associated with the JWT.
+ * @property {string} [epic.eci] - An optional string representing the Epic ECI (Epic Community Identifier).
  */
 export type JWT = {
   client_id: string;
@@ -31,39 +35,40 @@ export type JWT = {
 };
 
 /**
- * The function checks if an object is an instance of the JWT class by verifying if it has a client_id property.
+ * The function checks if an object is an instance of the JWT type by verifying if it has a client_id property.
  * @param {unknown} object - The `object` parameter is of type `unknown`, which means it can be any type.
- * @returns a boolean value.
+ * @returns {boolean} - A boolean value indicating whether the object is an instance of JWT.
  */
 export function instanceOfJWT(object: unknown): object is JWT {
   return (object as JWT).client_id !== undefined;
 }
 
 /**
-Represents the ClientFactory class for creating EMR clients.
-*/
+ * Represents the ClientFactory class for creating EMR clients.
+ */
 export default class ClientFactory {
   /**
    * The function `createEMRClient` creates an EMR client based on the specified launch type.
-   * @param {LAUNCH} launchType - The `launchType` parameter is an optional parameter of type `LAUNCH` that specifies the type of EMR launch. It has a default value
-   * of `LAUNCH.EMR`.
-   * @returns a Promise that resolves to an instance of the `BaseClient` class.
+   * @param {LAUNCH} launchType - The `launchType` parameter is an enum of type `LAUNCH` that specifies the type of EMR launch.
+   * @param {EMR} [emrType] - The type of EMR (e.g., EPIC, CERNER, etc.).
+   * @param {string | fhirclient.ClientState} [state] - The state or client state object for the FHIR client. This is used for STATEFUL launches.
+   * @returns {Promise<BaseClient>} - A Promise that resolves to an instance of the `BaseClient` class.
    */
   async createEMRClient(
-    launchType: LAUNCH.EMR | LAUNCH.STANDALONE,
+    launchType: LAUNCH,
     emrType?: EMR,
     state?: string | fhirclient.ClientState
   ): Promise<BaseClient> {
     const fhirClient = await this.createDefaultFhirClient(launchType, state);
-    return await this.createSmarterFhirClient(fhirClient, emrType);
+    return this.createSmarterFhirClient(fhirClient, emrType);
   }
 
   /**
-   * The function `createEMRClientBackend` creates an EMR client based on the specified launch type.
+   * The function `createEMRClientBackend` creates an EMR client for backend operations.
    * @param {IncomingMessage} req - The `req` parameter is an incoming message object that represents the request made by the client.
    * @param {ServerResponse} res - The `res` parameter is a server response object that represents the response sent by the server.
    * @param {FhirClientConfig} serverConfig - The `serverConfig` parameter is an object that contains the configuration for the FHIR client. It includes the server URL, token response, client ID, and token URI.
-   * @returns a Promise that resolves to an instance of the `BaseClient` class.
+   * @returns {Promise<BaseClient>} - A Promise that resolves to an instance of the `BaseClient` class.
    */
   async createEMRClientBackend(
     req: IncomingMessage,
@@ -80,7 +85,16 @@ export default class ClientFactory {
     return this.createSmarterFhirClient(fhirClient, EMR.NONE);
   }
 
-  private async createSmarterFhirClient(fhirClient: SubClient, emrType?: EMR) {
+  /**
+   * The function creates a smarter FHIR client based on the given FHIR client and EMR type.
+   * @param {SubClient} fhirClient - The FHIR client to base the smarter FHIR client on.
+   * @param {EMR} [emrType] - The type of EMR (e.g., EPIC, CERNER, etc.).
+   * @returns {Promise<BaseClient>} - A Promise that resolves to an instance of the `BaseClient` class.
+   */
+  private async createSmarterFhirClient(
+    fhirClient: SubClient,
+    emrType?: EMR
+  ): Promise<BaseClient> {
     const _emrType = emrType ?? ClientUtils.getEMRType(fhirClient);
     switch (_emrType) {
       case EMR.EPIC:
@@ -93,7 +107,7 @@ export default class ClientFactory {
         return new ECWClient(fhirClient);
       case EMR.MEDPLUM:
         return new MedplumClient(fhirClient);
-      case EMR.INFERNO: // Add Inferno client creation
+      case EMR.INFERNO:
         return new InfernoClient(fhirClient);
       case EMR.NONE:
       default:
@@ -103,9 +117,9 @@ export default class ClientFactory {
 
   /**
    * The function creates a default FHIR client based on the launch type.
-   * @param {LAUNCH} launchType - The `launchType` parameter is an enum type called `LAUNCH`. It represents the type of launch for the FHIR client. There are two
-   * possible values for `LAUNCH`:
-   * @returns a Promise that resolves to a SubClient object.
+   * @param {LAUNCH} launchType - The `launchType` parameter is an enum type that represents the type of launch for the FHIR client.
+   * @param {string | fhirclient.ClientState} [state] - The state or client state object for the FHIR client. This is used for STATEFUL launches.
+   * @returns {Promise<SubClient>} - A Promise that resolves to a SubClient object.
    */
   private async createDefaultFhirClient(
     launchType: LAUNCH,
@@ -116,10 +130,11 @@ export default class ClientFactory {
       case LAUNCH.STANDALONE:
         return FHIR.oauth2.ready();
       case LAUNCH.STATEFUL:
-        if (!state)
+        if (!state) {
           throw new Error(
             "state parameter is required when using the STATEFUL launch type"
           );
+        }
         return FHIR.oauth2.client(state);
       default:
         throw new Error("Unsupported provider for standalone launch");
@@ -127,14 +142,13 @@ export default class ClientFactory {
   }
 
   /**
-   * The function `getEmrEndpoints` returns the endpoints based on the EMR type obtained from the JWT.
-   * @param {JWT} jwt - The "jwt" parameter is a JSON Web Token (JWT) that is used for authentication and authorization purposes. It contains information about the
-   * user and their permissions.
-   * @returns an object of type EMR_ENDPOINTS.
+   * The function `getEmrEndpoints` returns the endpoints based on the EMR type obtained from the JWT or EMR.
+   * @param {EMR | JWT} object - The object representing either an EMR type or a JWT.
+   * @returns {EMR_ENDPOINTS} - An object containing the EMR endpoints.
    */
   private getEmrEndpoints(emrType: EMR): EMR_ENDPOINTS;
   private getEmrEndpoints(jwt: JWT): EMR_ENDPOINTS;
-  private getEmrEndpoints(object: unknown): EMR_ENDPOINTS {
+  private getEmrEndpoints(object: EMR | JWT): EMR_ENDPOINTS {
     switch (ClientUtils.getEmrTypeFromObject(object)) {
       case EMR.EPIC:
         return EpicClient.getEndpoints();
